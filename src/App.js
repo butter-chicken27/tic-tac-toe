741c8d89b2b2077e71cc673da4f64c30dc8ad29a
butter-chicken27 <agraj.srivastava60@gmail.com>
2020-12-01 09:11:49 +0530

    Initial grid set up

--- src/App.js
+++ src/App.js
@@ -1,15 +1,36 @@
-import {useState} from 'react';
+import {useEffect, useState} from 'react';
 import './App.css';
+import {Center, useColorMode, Box, Button, Grid} from '@chakra-ui/react';
+
 
 function App() {
   const [text, setText] = useState(0);
-  function handleChange(e, step){
-    setText(prevText => prevText + 1);
+  const { colorMode, toggleColorMode } = useColorMode()
+  useEffect(() => {
+    if(colorMode === 'light')
+      toggleColorMode();
+  }, []);
+  function handleClick(){
+    setText(prevText => {if(prevText === 1)
+    return 0;
+    return 1;
+  });
   }
   return (
     <div className="App">
-      <p>{text}</p>
-      <button onClick={handleChange}>Change</button>
+      <Center>
+        <Grid templateColumns="repeat(3, 1fr)" gap={1}>
+          <Box w="64px" h="64px" color='#00000' bg='#6200EA'><Button w="64px" h="64px" onClick={handleClick}>{text}</Button></Box>
+          <Box w="64px" h="64px" color='#00000' bg='#6200EA'><Button w="64px" h="64px" onClick={handleClick}>{text}</Button></Box>
+          <Box w="64px" h="64px" color='#00000' bg='#6200EA'><Button w="64px" h="64px" onClick={handleClick}>{text}</Button></Box>
+          <Box w="64px" h="64px" color='#00000' bg='#6200EA'><Button w="64px" h="64px" onClick={handleClick}>{text}</Button></Box>
+          <Box w="64px" h="64px" color='#00000' bg='#6200EA'><Button w="64px" h="64px" onClick={handleClick}>{text}</Button></Box>
+          <Box w="64px" h="64px" color='#00000' bg='#6200EA'><Button w="64px" h="64px" onClick={handleClick}>{text}</Button></Box>
+          <Box w="64px" h="64px" color='#00000' bg='#6200EA'><Button w="64px" h="64px" onClick={handleClick}>{text}</Button></Box>
+          <Box w="64px" h="64px" color='#00000' bg='#6200EA'><Button w="64px" h="64px" onClick={handleClick}>{text}</Button></Box>
+          <Box w="64px" h="64px" color='#00000' bg='#6200EA'><Button w="64px" h="64px" onClick={handleClick}>{text}</Button></Box>
+        </Grid>
+      </Center>
     </div>
   );
 }
